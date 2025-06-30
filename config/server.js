91cf606e0497eaba8c1466b80eb1da08543b0422
Admin <infoadministracia@protonmail.com>
2025-06-30 23:40:41 +0200

    Use APP_KEYS from environment variables

--- config/server.js
+++ config/server.js
@@ -1,5 +1,7 @@
 module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['fallbackKeyA', 'fallbackKeyB']),
+    keys: [767927a057a5918d48acc118afebe6dd, e12ebcf7ba0fd28121635e460dec52b6],
   },
 });
